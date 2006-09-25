ec3ee1e5afccaaeaa241ec40c3df0e95a29c9bd8
Brian Cassidy <bricas@cpan.org>
2006-09-25 14:22:41 +0000

    more fixes

--- demo/jsrpc.pl
+++ demo/jsrpc.pl
@@ -6,29 +6,29 @@ use Carp;
 use base qw( JavaScript::RPC::Server::CGI );
 
 sub add {
-	my $self = shift;
-	my @args = @_;
-	unless(
-		@args == 2 and
-		$args[ 0 ] =~ /^\d+$/ and
-		$args[ 1 ] =~ /^\d+$/
-	) {
-		croak( 'inputs must be digits only' ); 
-	}
-	return $args[ 0 ] + $args[ 1 ];
+    my $self = shift;
+    my @args = @_;
+    unless(
+        @args == 2 and
+        $args[ 0 ] =~ /^\d+$/ and
+        $args[ 1 ] =~ /^\d+$/
+    ) {
+        croak( 'inputs must be digits only' ); 
+    }
+    return $args[ 0 ] + $args[ 1 ];
 }
 
 sub subtract {
-	my $self = shift;
-	my @args = @_;
-	unless(
-		@args == 2 and
-		$args[ 0 ] =~ /^\d+$/ and
-		$args[ 1 ] =~ /^\d+$/
-	) {
-		croak( 'inputs must be digits only' );
-	}
-	return $args[ 0 ] - $args[ 1 ];
+    my $self = shift;
+    my @args = @_;
+    unless(
+        @args == 2 and
+        $args[ 0 ] =~ /^\d+$/ and
+        $args[ 1 ] =~ /^\d+$/
+    ) {
+        croak( 'inputs must be digits only' );
+    }
+    return $args[ 0 ] - $args[ 1 ];
 }
 
 package main;
@@ -37,3 +37,4 @@ use strict;
 
 my $server = MyJSRPC->new;
 $server->process;
+
